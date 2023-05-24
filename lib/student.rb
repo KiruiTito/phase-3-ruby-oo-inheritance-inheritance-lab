19b09aa08abaaff3589e7150faf84b8c4169cf3c
KiruiTito <titokirui402@gmail.com>
2023-05-24 15:34:59 +0300

    Completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
-
-end
+class Student < User
+    def initialize(first_name, last_name)
+      super(first_name, last_name)
+      @knowledge = []
+    end
+  
+    def learn(knowledge)
+      @knowledge << knowledge
+    end
+  
+    def knowledge
+      @knowledge
+    end
+  end
+  
--- lib/user.rb
+++ lib/user.rb
@@ -8,34 +8,5 @@ class User
     end
   end
   
-  # teacher.rb
-  class Teacher < User
-    KNOWLEDGE = [
-      "Physics",
-      "Math",
-      "Biology",
-      "History",
-      "Literature"
-    ]
   
-    def teach
-      KNOWLEDGE.sample
-    end
-  end
-  
-  # student.rb
-  class Student < User
-    def initialize(first_name, last_name)
-      super(first_name, last_name)
-      @knowledge = []
-    end
-  
-    def learn(knowledge)
-      @knowledge << knowledge
-    end
-  
-    def knowledge
-      @knowledge
-    end
-  end
   
